d952bc72dc88b60acf27ef34521d49a18b0067eb
YJ-Lazy <yuanjuan52025@gmail.com>
2026-08-24 19:08:13 +0800

    feat: add application class

---
+++ app/src/main/java/com/example/ssaidhookQQ/App.java
@@ -0,0 +1,13 @@
+package com.example.ssaidhookQQ;
+
+import android.app.Application;
+
+public class App extends Application {
+    public static final String PREF_NAME = "qqssaid_config";
+    public static final String KEY_ID = "android_id";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
